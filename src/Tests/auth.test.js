3476aa754fae29493606aa88f0b1b2139b63c79c
H.NGA.91 <h.nga.91@proton.me>
2026-07-21 03:00:55 +0200

    test 2

--- src/Tests/auth.test.js
+++ src/Tests/auth.test.js
@@ -96,7 +96,7 @@ describe("POST /api/auth/connexion", () => {
 	});
 
 	// ===== CAS BRUTE FORCE =====
-	it("devrait bloquer après 5 tentatives échouées", async () => {
+	it("devrait bloquer après 3 tentatives échouées", async () => {
 		// On envoie volontairement 3 mauvaises tentatives de suite
 		for (let i = 0; i < 3; i++) {
 			await request(app).post("/api/auth/connexion").send({
